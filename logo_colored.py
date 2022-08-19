b2a9f5553df16fe23412d3486c79a888bbdb0183
SaiedAlshahrani <s3ed.fala7@gmail.com>
2022-08-19 16:43:01 -0400

    commit new updates

--- logo_colored.py
+++ logo_colored.py
@@ -10,4 +10,3 @@ rich.print('''
           [dodger_blue2]▌│║▌║▌█║▌║║▌│║▌│║▌║▌█║[/][red]WIKISTATS-TO-CSV[/][dodger_blue2]║▌│█║▌│▌║│▌║║▌║▌█║▌║│▌[/]
           [dodger_blue2]▌│║▌║▌█║▌║║▌│║▌│║▌║▌█║[/][spring_green4]▌█║▌║│▌║│▌║│█║│▌[/][dodger_blue2]║▌│█║▌│▌║│▌║║▌║▌█║▌║│▌[/]
           ''')
-
