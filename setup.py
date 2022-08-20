c60dcc8cfc5f560064d1eac5710276643fb3a4d1
Saied Alshahrani <101026149+SaiedAlshahrani@users.noreply.github.com>
2022-08-20 14:41:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with codecs.open('README.md', 'r', 'utf8') as reader:
 
 setup(
     name = 'wikistats2csv',
-    version = '0.1.0',
+    version = '0.1.1',
     license = 'MIT',
     author = 'Saied Alshahrani',
     author_email = 's3ed.fala7@gmail.com',
